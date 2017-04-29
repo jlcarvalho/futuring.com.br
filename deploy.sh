78952d75723246561d34779af9cf6cf866184957
Jean Lucas de Carvalho <jeanlucasdecarvalho@gmail.com>
2017-04-29 01:51:20 -0300

    Atualização do script de deploy do now

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 npm i -g now
-URL=$(now deploy -t $ZEIT_TOKEN -e NODE_ENV=production)
-echo "running simple check on $URL"
+URL=$(now -t $ZEIT_TOKEN)
+echo "Running simple check on $URL"
 curl --silent -i -L $URL
 now alias $URL futuring.com.br -t $ZEIT_TOKEN
 now alias $URL futuring -t $ZEIT_TOKEN
