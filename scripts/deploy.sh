1ca408ead04348d5a35a027ee5e735322d26546e
Jean Lucas de Carvalho <jeanlucasdecarvalho@gmail.com>
2017-04-29 16:43:47 -0300

    Adição do bot que adiciona o link do deploys no commits e PRs

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,4 @@
 npm i -g now
 URL=$(now -t $ZEIT_TOKEN)
+echo "export URL=$URL" >> ~/.circlerc
 echo "<script>window.location.replace('$URL');</script><p>Deployment url: <a href='$URL'>$URL</a></p>" > $CIRCLE_ARTIFACTS/index.html
