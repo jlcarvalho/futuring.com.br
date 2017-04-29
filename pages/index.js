6789a8c799ac80cf5d7d0b2c5668b0ba6a8c0aa7
Jean Lucas de Carvalho <jeanlucasdecarvalho@gmail.com>
2017-04-29 00:12:43 -0300

    Criação dos testes para a home

--- pages/index.js
+++ pages/index.js
@@ -43,85 +43,90 @@ const Footer = styled.footer`
   }
 `
 
-const Works = [{
-  title: 'Meu Negócio',
-  // logo: '',
-  image: '../static/images/home-suave.png',
-  description: `
-    <p>The <b>Meu Negócio</b> application is an virtual assistant that provides Independent Beauty Consultants of Suave Fragrance the opportunity to create sales tickets, manage inventory and service her customers on the go from their mobile devices.</p>
-  `,
-  tags: ['Ionic', 'Angular', 'CouchDB', 'PouchDB', 'Node.js', 'now.sh'],
-  stores: {
-    apple: 'https://itunes.apple.com/us/app/suave-fragrance-meu-negócio/id1225991362',
-    google: 'https://play.google.com/store/apps/details?id=br.com.futuring.apps.suave'
+export default class extends React.Component {
+  static getInitialProps () {
+    return {
+      works: [{
+        title: 'Meu Negócio',
+        image: '../static/images/home-suave.png',
+        description: `
+          <p>The <b>Meu Negócio</b> application is an virtual assistant that provides Independent Beauty Consultants of Suave Fragrance the opportunity to create sales tickets, manage inventory and service her customers on the go from their mobile devices.</p>
+        `,
+        tags: ['Ionic', 'Angular', 'CouchDB', 'PouchDB', 'Node.js', 'now.sh'],
+        stores: {
+          apple: 'https://itunes.apple.com/us/app/suave-fragrance-meu-negócio/id1225991362',
+          google: 'https://play.google.com/store/apps/details?id=br.com.futuring.apps.suave'
+        }
+      }, {
+        title: 'Mini Receitas',
+        image: '../static/images/home-minireceitas.png',
+        description: `
+          <p><b>Mini Receitas</b> is an application that allows you to search for recipes through the ingredients that you have at hand. With thousands of possible combinations of filters.</p>
+        `,
+        tags: ['Ionic', 'Angular', 'CouchDB', 'PouchDB', 'Firebase'],
+        stores: {
+          google: 'https://play.google.com/store/apps/details?id=br.com.jeanlucas.apps.microreceitas'
+        }
+      }, {
+        title: 'Plan Kids',
+        image: '../static/images/home-plankids.png',
+        description: `
+          <p><b>Plan Kids</b> is an application for parents who wish to develop their children\'s potential through playful play that has been designed using psychopedagogy and coaching.</p>
+        `,
+        tags: ['Ionic', 'Angular', 'Parse Server', 'PouchDB', 'Docker', 'Heroku'],
+        stores: {
+          apple: 'https://itunes.apple.com/ca/app/plan-kids/id1147019060',
+          google: 'https://play.google.com/store/apps/details?id=br.com.futuring.apps.plankidsapp'
+        }
+      }]
+    }
   }
-}, {
-  title: 'Mini Receitas',
-  // logo: '',
-  image: '../static/images/home-minireceitas.png',
-  description: `
-    <p><b>Mini Receitas</b> is an application that allows you to search for recipes through the ingredients that you have at hand. With thousands of possible combinations of filters.</p>
-  `,
-  tags: ['Ionic', 'Angular', 'CouchDB', 'PouchDB', 'Firebase'],
-  stores: {
-    google: 'https://play.google.com/store/apps/details?id=br.com.jeanlucas.apps.microreceitas'
-  }
-}, {
-  title: 'Plan Kids',
-  // logo: '',
-  image: '../static/images/home-plankids.png',
-  description: `
-    <p><b>Plan Kids</b> is an application for parents who wish to develop their children\'s potential through playful play that has been designed using psychopedagogy and coaching.</p>
-  `,
-  tags: ['Ionic', 'Angular', 'Parse Server', 'PouchDB', 'Docker', 'Heroku'],
-  stores: {
-    apple: 'https://itunes.apple.com/ca/app/plan-kids/id1147019060',
-    google: 'https://play.google.com/store/apps/details?id=br.com.futuring.apps.plankidsapp'
-  }
-}]
 
-export default () => (
-  <div>
-    <HeroBackground>
-      <Section fullScreen>
-        <Header>
-          <Logo />
-          <Menu />
-        </Header>
+  render () {
+    return (
+      <div>
+        <HeroBackground>
+          <Section fullScreen>
+            <Header>
+              <Logo />
+              <Menu />
+            </Header>
 
-        <Main>
-          <HugeText>
-            We deliver digital <GradientText>experiences</GradientText>.
-          </HugeText>
-        </Main>
-      </Section>
-    </HeroBackground>
-    <WhiteBackground>
-      <Section padding id='work'>
-        <Container small>
-          <h2>Work</h2>
-          {
-            Works.map((work, key) => <Work key={key} {...work} rtl={!!(key % 2)} />)
-          }
-        </Container>
-      </Section>
-    </WhiteBackground>
-    <Section padding fullScreen id='hire'>
-      <Container small>
-        <h2>Hire Us</h2>
-        <HR />
-        <form action='https://formspree.io/contato@futuring.com.br' method='POST'>
-          <Input required type='text' name='name' id='name' placeholder='Name' />
-          <Input required type='email' name='email' id='email' placeholder='Email' />
-          <TextArea required name='project' id='project' placeholder='Tell us about your awesome project!' />
-          <Button type='submit'>Request a quote</Button>
-        </form>
-      </Container>
-    </Section>
-    <Footer>
-      <Container>
-        <p>Copyright © Futuring Ltd, 2017</p>
-      </Container>
-    </Footer>
-  </div>
-)
+            <Main>
+              <HugeText>
+                We deliver digital <GradientText>experiences</GradientText>.
+              </HugeText>
+            </Main>
+          </Section>
+        </HeroBackground>
+        <WhiteBackground>
+          <Section padding id='work'>
+            <Container small>
+              <h2>Work</h2>
+              {
+                this.props.works.map((work, key) => <Work className='work' key={key} {...work} rtl={!!(key % 2)} />)
+              }
+            </Container>
+          </Section>
+        </WhiteBackground>
+        <Section padding fullScreen id='hire'>
+          <Container small>
+            <h2>Hire Us</h2>
+            <HR />
+            <form action='https://formspree.io/contato@futuring.com.br' method='POST'>
+              <Input required type='text' name='name' id='name' placeholder='Name' />
+              <Input required type='email' name='email' id='email' placeholder='Email' />
+              <TextArea required name='project' id='project' placeholder='Tell us about your awesome project!' />
+              <Button type='submit'>Request a quote</Button>
+            </form>
+          </Container>
+        </Section>
+        <Footer>
+          <Container>
+            <p>Copyright © Futuring Ltd, 2017</p>
+          </Container>
+        </Footer>
+      </div>
+    )
+  }
+}
