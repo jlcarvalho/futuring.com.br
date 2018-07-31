d86447782ddd9c28e22d4622853426abc5f5c289
Jean Lucas de Carvalho <jeanlucasdecarvalho@gmail.com>
2018-07-31 00:01:17 -0300

    Remove user-scalable=no

--- pages/_document.js
+++ pages/_document.js
@@ -18,7 +18,7 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           <title>Futuring</title>
-          <meta name='viewport' content='width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no' />
+          <meta name='viewport' content='width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0' />
 
           <meta name='description' content='We deliver digital experiences.' />
 
